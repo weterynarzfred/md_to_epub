e4d70f4864f8cb6c1ba032f1e4a4b5c11ba5c409
weterynarzfred <weterynarzfred@gmail.com>
2022-08-02 21:57:29 +0200

    add a progress bar

--- src/make_epub.js
+++ src/make_epub.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const cliProgress = require('cli-progress');
 
 const { SOURCE_PATH } = require('./constants');
 const generateBookData = require('./functions/generateBookData');
@@ -21,10 +22,29 @@ async function saveOutputFiles(bookData) {
     fs.mkdirSync('./output');
   }
 
+  const length = Object.keys(bookData).length;
+  const multiBar = new cliProgress.MultiBar({
+    format: '[{bar}] {percentage}% | ETA: {eta}s | {value}/{total} | {filename}'
+  }, cliProgress.Presets.shades_classic);
+
+  const bar1 = multiBar.create(length, 0);
+  const bar2 = multiBar.create(length, 0);
+
+  let index = 0;
   for (const title in bookData) {
     await makeEpub(bookData[title]);
-    convertToPdf(bookData[title], 'epub');
+    bar1.increment(1, { filename: title });
+    const promise = convertToPdf(bookData[title], 'epub');
+    promise.then(() => {
+      index++;
+      bar2.increment(1, { filename: title });
+      if (index === length) {
+        bar2.update(length, { filename: 'pdf files done' });
+        multiBar.stop();
+      }
+    });
   }
+  bar1.update(length, { filename: 'epub files done' });
 }
 
 const markdownFiles = readInputFiles();
